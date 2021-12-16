d450e4ee4c860f5732b20e4ea516ea6969ca1b65
Kimberly Eslami <36568548+kimberlyeslami@users.noreply.github.com>
2021-12-16 14:58:59 +0000

    added next step handler

--- main.py
+++ main.py
@@ -8,13 +8,9 @@ replies = []
 API_KEY = os.environ['API_KEY']
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(commands=['Greet'])
-def greet(message):
-  bot.reply_to(message, "Hey! Hows it going?")
-
 @bot.message_handler(commands=['Hello'])
 def hello(message):
-  bot.send_message(message.chat.id, "Hello!")
+  bot.send_message(message.chat.id, "Hello! To start use the command /Play")
 
 @bot.message_handler(commands=["Play"])
 def play(message):
@@ -27,19 +23,40 @@ def query1(message):
   print(replies)
   print(data)
   if replies != None:
-    data1 = data[(data.Type=="Animal")]
-    print("It worked")
-    print(data1)
-  @bot.message_handler(func=lambda message: True)
-  def query2(message):
-    bot.send_message(message.chat.id,"Is it large?")
-    print(replies)
-    data2 = data1[(data.Size=="Large")]
-    if len(data2.index) == 1:
-      ans = data2.iloc[0].Item
-      print(ans)
-      bot.send_message(message.chat.id, ans)
-  query2(message)
+    if replies[0] == "Yes":
+      animal_sort = data[(data.Type=="Animal")]
+      print("Here is the data sorted by Animal")
+      print(animal_sort)
+    elif replies[0] == "No":
+      vegetable_sort = data[(data.Type=="Vegetable")]
+      print("Here is the data sorted by Vegetable")
+      print(vegetable_sort)
+  msg = bot.reply_to(message, 'is it Large?')
+  bot.register_next_step_handler(msg, query2)
+def query2(message):
+  # bot.send_message(message.chat.id,"Is it large?")
+  response = message.text
+  replies.append(response)
+  print(replies)
+  data1 = data[(data.Type=="Animal")]
+  if replies[1] == "Yes":
+      size_large = data1[( data.Size=="Large")]
+      print(size_large)
+      if len(size_large.index) == 1:
+        ans = size_large.iloc[0].Item
+        print(ans)
+        bot.send_message(message.chat.id, f"I think its an {ans}. Am I right?")
+  elif replies[1] == "No":
+      vegetable_sort = data[(data.Type=="Vegetable")]
+      print("Here is the data sorted by Vegetable")
+      print(vegetable_sort)
+  # if len(size_large.index) == 1:
+  #   ans = size_large.iloc[0].Item
+  #   print(ans)
+  #   bot.send_message(message.chat.id, ans)
+
+   
+bot.polling()
 
 #     # if replies[message.chat.id].filter=="Type":
 #     #   if response == "Yes":
@@ -71,4 +88,3 @@ def query1(message):
 # #     bot.send_message(message.chat.id, "is it Green?")
 # # # #filter through the csv file if it is an animal, filter by animal. 
     
-bot.polling()
